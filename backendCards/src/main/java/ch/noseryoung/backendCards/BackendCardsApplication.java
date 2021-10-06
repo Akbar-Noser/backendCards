8f8fa2b4b045cdbc899e37272b808ac9bd3f33bf
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2021-10-06 12:08:22 +0200

    initialize

---
+++ backendCards/src/main/java/ch/noseryoung/backendCards/BackendCardsApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.backendCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCardsApplication.class, args);
+	}
+
+}
---
+++ backendCards/src/test/java/ch/noseryoung/backendCards/BackendCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.backendCards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
